6e36a74543329ed0ac981d3e7f0ba31fefe035cf
Akshat Mittal <akshatmittal2506@gmail.com>
2025-03-19 23:38:20 +0530

    fix: api routes

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,5 +1,4 @@
 import { HTTP, serverBaseUrl } from "@/constants";
-import { Logger } from "@/log";
 import { ApiRequest, ApiResponse } from "@/types";
 import axios from "axios";
 import { NextApiHandler } from "next";
@@ -7,13 +6,11 @@ import { NextApiHandler } from "next";
 const handler: NextApiHandler = async (req: ApiRequest, res: ApiResponse) => {
 	try {
 		const headers = { cookie: req.headers.cookie };
-		Logger.debug("health headers", headers);
 		const response = await axios.get(`${serverBaseUrl}/api/health`, {
 			headers,
 		});
 		return res.status(response.status).json(response.data);
 	} catch (err: any) {
-		Logger.error(err);
 		return res
 			.status(err?.response?.status || HTTP.status.INTERNAL_SERVER_ERROR)
 			.json(
